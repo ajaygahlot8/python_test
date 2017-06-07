e89d0865ef9eccdc2be5f0a491b4e143cd1c5175
cuzok <cuzook@gmail.com>
2017-06-07 19:22:19 +0530

    Servlet notes added

---
+++ Java_Web_App.java
@@ -0,0 +1,73 @@
+1) Basic of web application
+  // When you hit google.com in browser ... request is sent to web server
+  // from there http request converts into servlet request then response is created
+  // then response converted into http response and sent back to client
+
+2) Basic request flow in java web application
+
+3)web.xml
+
+src>main>webapp>WEB-INF>web.xml
+//it is the first file java web application looks into when it gets the request
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...................
