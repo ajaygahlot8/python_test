b672218ed8843147389256cf3047e7ea47500036
cuzok <cuzook@gmail.com>
2017-08-14 19:06:16 +0530

    removed unnecessary files

--- Servlet_Notes.java
+++
@@ -1,9 +0,0 @@
-1) Web Server > Web Container > Web Application
-
-2) Tomcat is servlet container . Web Container or Servlet Container or Servlet Engine :
- is used to manage the components like servlets, JSP.It is a part of the web server.
-
-3)Web Server or HTTP Server: a server which is capable of handling HTTP request send by a client
-and respond back with a HTTP response.
-
-4)
